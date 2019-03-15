acf37a3df33374ea3394fc382678f7a281647e07
zhiqiangfang <zhiqiang.fang@foxmail.com>
2019-03-15 21:42:02 +0800

    update

--- pic/chapter_09/problem_7.c
+++ pic/chapter_09/problem_7.c
@@ -4,12 +4,29 @@ void insertString(char source[], char text[], int index)
 {
     int i, lens = 0, lent = 0;
 
-    for (i = 0; source[index + i] != '\0'; ++i);
-    lens = i - 1;
+    for (i = 0; source[i] != '\0'; ++i);
+    lens = i;
 
     for (i = 0; text[i] != '\0'; ++i);
-    lent = i - 1;
+    lent = i;
 
+    for (i = lens; i >= index; --i)
+        source[i + lent] = source[i];
+    
+    for (i = index; i < index + lent; ++i)
+        source[i] = text[i - index];
+}
 
-        
+int main(void)
+{
+    char source[] = "the wrong son";
+    char text[] = "per";
+    int index = 10;
+    void insertString(char source[], char text[], int);
+
+    insertString(source, text, index);
+
+    printf ("%s\n", source);
+
+    return 0;
 }
